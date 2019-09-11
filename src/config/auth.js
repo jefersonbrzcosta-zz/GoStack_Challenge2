d090d4c5d1d8d7c4cab095f31f9e71d7406a73be
Jeferson Costa <jefersonbrz.costa@gmail.com>
2019-09-11 17:42:22 -0300

    new

---
+++ src/config/auth.js
@@ -0,0 +1,5 @@
+export default {
+  user: "promebackend",
+  pass: "goBarber3052",
+  database: "promebackend"
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "mysql",
+  host: "promebackend.mysql.dbaas.com.br",
+  username: "promebackend",
+  password: "goBarber3052!",
+  database: "promebackend",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
